ff8b4bd98c5217f5909d49778b65a0642262981a
root <root@ip-172-31-21-208.ap-southeast-1.compute.internal>
2018-07-18 15:58:38 +0000

    ping

---
+++ world.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+echo "Hello world"
+# hai hw r u ra mama.:
+echo $BASH
+echo the version is $BASH_VERSION
+echo home $HOME
+echo my $PWD
+
+
+name=kiran
+val=10
+echo The name is $name
+echo value $val
+
+echo "enter name : "
+read name
+echo "enterd name :$name"
+
+echo "enter names : "
+read name1 name2 name3
+echo "enterd names :$name"
+
+echo "enter name : "
+read -p name
+echo "enterd name $name"
+
+read -p "username : "
+read -sp "password : "
+echo "username : $username"
+echo "passwd : $password"
+~
+~
+
